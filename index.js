30e7fe310f63cd60420182199a478e8251508d05
Daria-Gurova <darigurrova@yandex.ru>
2023-10-05 21:48:05 +0400

    bla

---
+++ index.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector('#hamburger');
+console.log(hamburger);
+const overlay = document.querySelector('#overlay');
+console.log(overlay);
+const body = document.querySelector('body');
+const link = document.querySelectorAll('.overlay .menu__link');
+console.log(link);
+
+function toggleMenu() {
+    //e.preventDefault();
+    hamburger.classList.toggle('hamburger--active');
+    overlay.classList.toggle('overlay--active');
+    body.classList.toggle('body--active-menu');
+};
+
+link.forEach(function(element) {
+    element.addEventListener('click', toggleMenu);
+});
+
+hamburger.addEventListener('click', toggleMenu);
