04ea4b0c09e9e41510ad6a31d9857345ed4f6865
Daria-Gurova <darigurrova@yandex.ru>
2023-10-06 10:55:25 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const body = document.querySelector('body');
 const link = document.querySelectorAll('.overlay .menu__link');
 console.log(link);
 
-function toggleMenu(evt) {
-    evt.preventDefault();
+function toggleMenu() {
+    //evt.preventDefault();
     hamburger.classList.toggle('hamburger--active');
     overlay.classList.toggle('overlay--active');
     body.classList.toggle('body--active-menu');
@@ -17,4 +17,4 @@ link.forEach(function(element) {
     element.addEventListener('click', toggleMenu);
 });
 
-hamburger.addEventListener('click', toggleMenu);
+hamburger.addEventListener('click', (event) =>{event.preventDefault(), toggleMenu()});
