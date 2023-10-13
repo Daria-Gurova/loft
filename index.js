c4ccf7e1c12ac431b7bd994d8e0c8e9d7ba1a3e2
Daria-Gurova <darigurrova@yandex.ru>
2023-10-14 01:55:22 +0400

    HW 4

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 // Здесь делаем для раскрытия меню при нажатии на бургер (и закрытия меню при нажатии на крестик)
 const hamburger = document.querySelector('#hamburger');
-console.log(hamburger);
 const overlay = document.querySelector('#overlay');
-console.log(overlay);
 const body = document.querySelector('body');
 const link = document.querySelectorAll('.overlay .menu__link');
-console.log(link);
 
 function toggleMenu() {
     //evt.preventDefault();
@@ -46,16 +43,20 @@ const openItem = item => {
     const contentBlock = container.find(".team__content");
     const textBlock = contentBlock.find(".team__content-block");
     const reqHeight = textBlock.height();
+    const red = container.find(".team__red");
     
     container.addClass("active");
     contentBlock.height(reqHeight);
+    red.css("transform","rotate(180deg)");
 }
 
 const closeItem = container => {
+    const red = container.find(".team__red");
     const items = container.find(".team__content");
     const itemContainer = container.find(".team__item");
     itemContainer.removeClass("active");
     items.height(0);
+    red.css("transform","rotate(360deg)");
 }
 
 $(".team__title").click(e => {
@@ -66,6 +67,7 @@ $(".team__title").click(e => {
 
     if (elemContainer.hasClass("active")) {
         closeItem(container);
+    
     } else {
         closeItem(container); 
         openItem($this);
@@ -91,11 +93,94 @@ $("#right").click(e => {
 
 // Здесь для формы
 
-$(".form").submit(e =>  {
+const modal = document.querySelector(".modal"); // все модальное окно
+const modalOpen = document.querySelector("#modal-open") // кнопка Заказать
+const modalClose = modal.querySelector(".app-close-modal"); // кнопка Закрыть
+
+const closeModalByEsc = event => {
+    if (event.key === 'Escape') { 
+        const modalActived = document.querySelector('.modal_active'); 
+        closePopup(modalActived); 
+   };
+};
+
+
+// функция, которая добавляет модификатор modal_active, чтобы открыть модальное окно
+function openPopup(modalOpen) {
+    modalOpen.classList.add('modal_active');
+};
+
+//функция, которая удаляет модификатор modal_active, чтобы закрыть модальное окно
+function closePopup(modalClosed) {
+    modalClosed.classList.remove('modal_active');
+};
+
+// Открытие модального онка при нажатии на Заказать
+modalOpen.addEventListener('click', function() {
+    openPopup(modal);
+    document.addEventListener('keydown', closeModalByEsc); 
+});
+
+// Закрытие модального окна при нажатии на Закрыть
+
+modalClose.addEventListener('click', function() {
+    closePopup(modal);
+    document.removeEventListener('keydown', closeModalByEsc);
+  });
+  
+// Закрытие попап по нажатию Esc
+  
+document.addEventListener('click', function(event) { 
+    if (event.target.classList.contains('modal')) { 
+        closePopup(event.target); 
+    } 
+});
+  
+// Закрытие попап по клику на overlay
+  
+document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('modal')) {
+        closePopup(event.target);
+    }
+});
+
+// Валидация формы
+
+const validateFields = (form, fieldsArray) => {
+    fieldsArray.forEach(field => {
+        field.removeClass('input-error');
+        if (field.val().trim() === "") {
+            field.addClass('input-error');
+        }
+    })
+
+    const errorFields = form.find('.input-error');
+
+    return errorFields.length === 0;
+}
+
+$('.form').submit(e => {
     e.preventDefault();
 
-    $.fancybox.open({
-        src: "#modal",
-        type: "inline"
-    })
+    const form = $(e.currentTarget);
+    const name = form.find("[name = 'name']");
+    const phone = form.find("[name = 'phone']");
+    const comment = form.find("[name = 'comment']");
+    const to = form.find("[name = 'to']");
+
+    const isValid = validateFields(form, [name, phone, comment, to]);
+
+    if (isValid) {
+        $.ajax({
+            url: "https://webdev-api.loftschool.com/sendmail",
+            method: "post",
+            data: {
+                name: name.val(),
+                phone: phone.val(),
+                comment: comment.val(),
+                to: to.val()
+            }
+        })
+    }
 });
+  
